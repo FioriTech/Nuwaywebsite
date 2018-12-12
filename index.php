49acd84ff5aca9c915bfcb562dccb267171a8432
lovesharma1995 <45064534+lovesharma1995@users.noreply.github.com>
2018-12-12 20:47:41 +0530

    index page

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Fiori Technologies</title>
+    <link href="css_file/common.css" type="text/css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+    <div class="image">
+        <header>
+      <img src="images/logo.png" alt="FioriTech Logo" height="90px" class="logo">
+	  <?php include 'php_files/common_files/menu.php' ?>
+	  </header>
+        <div class="clear"></div>
+        <div id="innercontent">
+            <div class="innertext">
+                <h1>Smart Solutions
+                for Intelligent Enterprise</h1>
+                <p>Helping Business Transform in a Digital World</p>
+                <a href="#" id="learnmore">Request Demo</a>
+            </div>
+            <div id="innerimage">
+                <img src="images/d39f1a_d60c9ebb3234456481267d5d59b9cf9b_mv2.gif" alt="website analytics" width="550px" height="240px">
+            </div>
+        </div>
+		<div class="clear"></div>
+        <div class="design">
+            <p>Nuway Solutions</p>
+        </div>
+    </div>
+    <div class="clear"></div>
+    <div class="paragraph">
+        <h1>Who We Are</h1>
+        <p>Nuway Solutions designs and deploys Digital Solutions for Fortune 2000 Companies. Nuway's heritage exhibits innovation and strategic foresight with respect to emerging technologies, maximizing its customer’s investments.<br><br>
+        Nuway touches all things Digital, focusing on Intelligent Enterprises, and solutions designed to transform Operational Efficiencies.</p>
+		
+		<div class="fb-page" data-href="https://www.facebook.com/NUWAYsolutions/" height="180px" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/NUWAYsolutions/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/FioriTech/">Nuway Solutions</a></blockquote></div>
+    </div>
+	
+    <div class="service">
+        <h1 id="serviceheading">Our Capabilities</h1>
+        <div class="subservice">
+            <p>Customer Experience</p>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="5.137445449829102 5.637445449829102 188.72511291503906 188.72511291503906" role="img" preserveAspectRatio="xMidYMid meet" style="stroke-width: 0px;"><g><path class="st0" d="M149.8,80l37.8-37.8c8.3-8.3,8.3-21.9,0-30.3s-21.9-8.3-30.3,0l-37.8,37.8c-8.3,8.3-8.3,21.9,0,30.3
+        S141.4,88.3,149.8,80z"></path><path class="st0" d="M49.2,120l-37.8,37.8c-8.3,8.3-8.3,21.9,0,30.3s21.9,8.3,30.3,0l37.8-37.8c8.3-8.3,8.3-21.9,0-30.3
+        S57.6,111.7,49.2,120z"></path><path class="st0" d="M149.8,120c-8.3-8.3-21.9-8.3-30.3,0s-8.3,21.9,0,30.3l37.8,37.8c8.3,8.3,21.9,8.3,30.3,0s8.3-21.9,0-30.3
+        L149.8,120z"></path><path class="st0" d="M41.7,12c-8.3-8.3-21.9-8.3-30.3,0s-8.3,21.9,0,30.3L49.2,80c8.3,8.3,21.9,8.3,30.3,0s8.3-21.9,0-30.3L41.7,12
+        z"></path></g></svg>
+        <div class="clear"></div>
+            <span>
+                We have deep heritage and experise in all things Cloud CRM. Whether it's SAP C/4HANA, Salesforce, Dynamics 365, NetSuite or more, we're leading the Customer 360 revolution of today's evolving "CX" landscape
+            </span>
+            <a href="#">+Learn More</a>
+        </div>
+        <div class="subservice">
+            <p>Intelligent Enterprise</p>
+            <svg viewBox="44 44.99977493286133 111.99984741210938 110.00022888183594" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="color" role="img" preserveAspectRatio="xMidYMid meet" style="">
+    <g>
+        <path d="M93.299 155C66.116 155 44 132.927 44 105.797s22.116-49.203 49.299-49.203c5.463 0 10.83.887 15.954 2.634a3.371 3.371 0 0 1 2.104 4.28l-12.72 37.122 37.567-11.547a3.376 3.376 0 0 1 4.221 2.229 49.128 49.128 0 0 1 2.172 14.485c0 27.13-22.115 49.203-49.298 49.203zm0-91.666c-23.46 0-42.545 19.049-42.545 42.463S69.84 148.26 93.299 148.26s42.545-19.049 42.545-42.463c0-3.127-.339-6.222-1.01-9.241l-40.542 12.461a3.375 3.375 0 0 1-4.189-4.312l13.725-40.063a42.74 42.74 0 0 0-10.529-1.308z" fill="#0E4828" data-color="1"></path>
+        <path d="M155.965 75.444c-4.383-14.221-15.426-25.586-29.539-30.402a.795.795 0 0 0-1.007.493l-14.862 43.379a.792.792 0 0 0 .983 1.012l43.899-13.494a.79.79 0 0 0 .526-.988z" fill="#E98866" data-color="2"></path>
+        </g>
+        </svg>
+        <div class="clear"></div>
+            <span>
+                Run your business more effectively and efficiently using powerful yet nimble reporting analytics and tools. Start your journey towards modernizing your data assets to achieve the outcomes you want faster.<br>
+            </span>
+            <a href="#">+Learn More</a>
+        </div>
+
+        <div class="subservice">
+            <p>Digital Core & ERP</p>
+        <svg viewBox="9.999000549316406 16 180 167.99900817871094" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="color" role="img" preserveAspectRatio="xMidYMid meet" style="">
+        <g>
+        <path d="M169.31 83.848c-4.858 0-9.355 1.8-12.954 4.499L128.92 62.522c4.498-4.769 7.556-11.698 7.556-18.897C136.476 28.328 124.152 16 108.77 16S81.064 28.238 81.064 43.535c0 11.698 7.556 21.956 17.811 25.825l-3.328 21.956c-7.394.288-13.045 6.42-12.249 14.019a7.016 7.016 0 0 1-.075.108l-38.231 9.628c-2.699-6.569-9.085-11.158-16.912-11.158-9.985 0-18.081 8.099-18.081 17.997s8.096 17.997 18.081 17.997c6.297 0 11.784-2.969 15.112-7.829l58.201 28.255c-.63 1.8-.9 3.599-.9 5.669 0 9.898 8.096 17.997 18.081 17.997s18.081-8.099 18.081-17.997-8.096-17.997-18.081-17.997c-.9 0-1.799 0-2.699.27l-11.154-35.184c2.069-1.8 3.328-4.499 3.958-7.199l40.66.63c1.169 10.258 9.625 17.997 20.15 17.997 11.154 0 20.51-8.998 20.51-20.426s-9.534-20.245-20.689-20.245zM108.501 70.71c6.027 0 11.784-2.07 16.282-5.399l27.796 26.185c-1.799 2.7-3.328 5.669-3.598 8.998l-40.66-.27c-1.169-3.869-3.958-7.199-7.826-8.728l3.328-21.326c1.439.54 2.879.54 4.678.54zm1.979 79.096c-3.058 1.53-5.397 3.599-7.196 6.299L45.442 127.58c.798-2.28 1.018-4.561.843-7.39l.056-.078 38.861-9.898c2.069 3.869 6.297 6.299 11.154 6.299 1.529 0 3.058-.27 4.228-.63l9.896 33.923z" fill="#F79421" data-color="1"></path>
+        </g>
+        </svg>
+        <div class="clear"></div>
+            <span>
+               Let us help you evaluate your core ERP systems, and learn about the latest  In-memory processing that offers efficiency, and easy connections to big data and transactional processes, to drive growth and innovation. <br>
+            </span>
+            <a href="#">+Learn More</a>
+        </div>
+
+        <div class="subservice">
+            <p>Innovative Technology</p>
+        <svg viewBox="49.900001525878906 19.900001525878906 99.99999237060547 160.20001220703125" xmlns="http://www.w3.org/2000/svg" data-type="color" role="img" preserveAspectRatio="xMidYMid meet" style="">
+        <g>
+        <path d="M130 155.1v10c0 2.8-2.2 5-5 5h-5c0 5.5-4.5 10-10 10H90c-5.4 0-10-4.5-10-10h-5c-2.8 0-5-2.2-5-5v-10c0-2.8 2.2-5 5-5h50c2.8 0 5 2.3 5 5zm19.9-85c0 5.2-.8 10.3-2.3 15l-18.2 48.1c-1.3 4-5.1 7-9.6 7h-40c-4.5 0-8.3-2.9-9.6-7L52.2 85c-1.5-4.7-2.3-9.8-2.3-15 0-27.6 22.3-50.1 50-50.1 27.7.1 50 22.4 50 50.2zM105 40c0-2.8-2.2-5-5-5-19.2 0-34.9 15.7-34.9 35 0 2.8 2.2 5 5 5s5-2.2 5-5c0-13.7 11.3-25 25-25 2.7 0 4.9-2.3 4.9-5z" fill="#f9ec00" data-color="1"></path>
+        </g>
+        </svg>
+        <div class="clear"></div>
+            <span>
+               Let our expertise help you get ahead of the curve. To remain at the forefront, you need to quickly adapt and embrace the latest technological advancements, and that's exactly what we implement for our clients. <br>
+            </span>
+            <a href="#">+Learn More</a>
+        </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="clear"></div>
+    <div>
+        
+    </div>
+    
+    <?php include 'php_files/common_files/footer.php' ?>	
+</body>
+</html>
